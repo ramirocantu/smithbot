a75512b2517b1f622a65264fd2addfdf502a184a
Ramiro Ivan Cantu <ramiro.cantu.ramirez@outlook.com>
2020-06-15 16:36:40 -0500

    Adds basic commands with http trigger. TO-DO add authentication

--- smithbot.js
+++ smithbot.js
@@ -1,15 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-    }
-});
+const https = require('https');
 
 // DO NOT EDIT, logs into discord with Bot Token
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+    if(message.content.substring(0, 1) == '-') {
+        var args = message.content.substring(1).split(' ');
+        var cmd = args[0];
+        switch (cmd) {
+            case 'help':
+                message.reply('Type -start to start the server');
+                break;
+            case 'start':
+                message.reply(startServer());
+                break;
+        }
+    }
+});
+
+function startServer () {
+    https.get(process.env.APIurl)
+    return 'Server is starting.';
+}
